df5ddb0e5c826eb4f7213dae1fc7eb02c508c0d1
jkob <jakob.lofgren1@gmail.com>
2018-01-13 15:31:47 +0100

    added node

---
+++ node.go
@@ -0,0 +1,12 @@
+package main
+
+// Node : Represents a pixel on the image
+type Node struct {
+	up      *Node
+	down    *Node
+	left    *Node
+	right   *Node
+	isWall  bool
+	checked bool
+	parent  *Node
+}
